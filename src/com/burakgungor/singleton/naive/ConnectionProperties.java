30469c41b2ab21f54d475686ecc40ed3797afa60
gungor <atakan.gungor@pia-team.com>
2020-09-05 22:54:56 +0300

    singleton classas real world anology

--- src/com/burakgungor/singleton/naive/ConnectionProperties.java
+++ src/com/burakgungor/singleton/naive/ConnectionProperties.java
@@ -2,7 +2,7 @@ package com.burakgungor.singleton.naive;
 
 import java.util.Objects;
 
-public class Singleton {
+public class ConnectionProperties {
     /**
     • Make the default constructor private, to prevent other objects from using the new operator with the Singleton class.
     • Create a static creation method that acts as a constructor. Under the hood, this method calls the private constructor to
@@ -10,16 +10,20 @@ public class Singleton {
     If your code has access to the Singleton class, then it’s able to call the Singleton’s static method. So whenever that
     method is called, the same object is always returned.
     */
-    public static Singleton instance;
-    public String value;
+    public static ConnectionProperties instance;
+    public String databaseUrl;
+    public String username;
+    public String password;
 
-    private Singleton(String value) {
-        this.value = value;
+    private ConnectionProperties(String databaseUrl, String username, String password) {
+        this.databaseUrl = databaseUrl;
+        this.username = username;
+        this.password = password;
     }
 
-    public static Singleton getInstance(String value) {
+    public static ConnectionProperties getProperties(String databaseUrl, String username, String password) {
         if (Objects.isNull(instance)) {
-            instance = new Singleton(value);
+            instance = new ConnectionProperties(databaseUrl,username,password);
         }
         return instance;
     }
--- src/com/burakgungor/singleton/naive/Main.java
+++ src/com/burakgungor/singleton/naive/Main.java
@@ -3,43 +3,43 @@ package com.burakgungor.singleton.naive;
 public class Main {
     public static void main(String[] args) {
         singleThread();
-        System.out.println("---");
+        System.out.println("---Multi");
         multiThread();
 
     }
 
     private static void multiThread() {
-        Thread threadFoo = new Thread(new ThreadFoo());
-        Thread threadBar = new Thread(new ThreadBar());
-        threadFoo.start();
-        threadBar.start();
+        Thread threadConn = new Thread(new ThreadConn());
+        Thread threadConn2 = new Thread(new ThreadConn2());
+        threadConn.start();
+        threadConn2.start();
     }
 
     private static void singleThread() {
-        Singleton objectFoo = getSingleton("FOO");
-        Singleton objectBar = getSingleton("BAR");
-        System.out.println(objectFoo.value);
-        System.out.println(objectBar.value);
+        ConnectionProperties connection = getConnection("localhost:8080", "admin", "admin");
+        ConnectionProperties connection2 = getConnection("localhost:9999", "user", "user");
+        System.out.println(connection.databaseUrl + " , " + connection.username + " , " + connection.password + "\n" +
+                connection2.databaseUrl + " , " + connection2.username + " , " + connection2.password);
     }
 
-    private static Singleton getSingleton(String value) {
-        return Singleton.getInstance(value);
+    private static ConnectionProperties getConnection(String databaseUrl, String username, String password) {
+        return ConnectionProperties.getProperties(databaseUrl, username, password);
     }
 
-    private static class ThreadBar implements Runnable {
+    private static class ThreadConn2 implements Runnable {
         @Override
         public void run() {
-            Singleton singleton = getSingleton("BAR");
-            System.out.println(singleton.value);
+            ConnectionProperties connectionProperties = getConnection("localhost:8080", "admin", "admin");
+            System.out.println(connectionProperties.databaseUrl + " , " + connectionProperties.username + " , " + connectionProperties.password);
 
         }
     }
 
-    private static class ThreadFoo implements Runnable {
+    private static class ThreadConn implements Runnable {
         @Override
         public void run() {
-            Singleton singleton = getSingleton("FOO");
-            System.out.println(singleton.value);
+            ConnectionProperties connectionProperties = getConnection("localhost:9999", "user", "user");
+            System.out.println(connectionProperties.databaseUrl + " , " + connectionProperties.username + " , " + connectionProperties.password);
         }
     }
 }
